ff22e2e1111f289a69040b28868245dafc76505f
HumayunKabir1981 <callmilton.com>
2026-05-04 00:44:31 +0600

    versel localhost conflict resolv

--- src/app/(auth)/animals/page.jsx
+++ src/app/(auth)/animals/page.jsx
@@ -1,22 +1,15 @@
 
 
 import AnimalsClient from "@/components/AnimalsClient ";
+import animals from "@/../public/animals.json";
 
-export default async function AnimalsPage() {
-
-  const res = await fetch('http://localhost:3000/animals.json', {
-    cache: "no-store",
-  });
-
-  const animals = await res.json();
-
+export default function AnimalsPage() {
   return (
     <div className="bg-slate-200">
       <h1 className="text-4xl font-bold py-6 text-center">
         All Animals
       </h1>
 
-      {/* 👉 client component এ data pass */}
       <AnimalsClient animals={animals} />
     </div>
   );
